10cafd03320a29020623cb4acdb80f59fbfbbfe7
Karthik kumar <mkarthikkumar19@gmail.com>
2020-08-14 12:38:41 +0530

    project initialized

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  
+  StyleSheet,
+  
+  View,
+  Text,
+
+} from 'react-native';
+
+
+
+const App = props => {
+  return(
+    <View style={styles.screen}>
+      <Text>app.js</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+ screen:{
+   flex:1
+ }
+
+});
+
+export default App;
